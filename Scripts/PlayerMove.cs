ee454639028d6634fdb3c0656e0f9cd0a64d22b4
MathiasVeraM <131045650+MathiasVeraM@users.noreply.github.com>
2025-04-06 23:27:55 -0500

    Subo lo mas que puedo del proyecto
    No me permite subir el proyecto completo profe, esto es lo mas que puedo subir

---
+++ Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+    private Vector3 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal"); // A/D o ←/→
+        float moveZ = Input.GetAxis("Vertical");   // W/S o ↑/↓
+
+        moveDirection = new Vector3(moveX, 0, moveZ).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
+    }
+}
